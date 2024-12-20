62ab68bfa72ae00e9d93cf760819a12db67559e6
Dmytro Yaremyshyn <dmytroy@dxspark.com>
2024-12-20 21:57:26 +0000

    Using empty view in new appointment view when there is no services

--- StudioManager/StudioManager/UI/Views and Controllers/NewAppointmentView.swift
+++ StudioManager/StudioManager/UI/Views and Controllers/NewAppointmentView.swift
@@ -11,55 +11,59 @@ public struct NewAppointmentView: View {
     @StateObject var viewModel: NewAppointmentViewModel
 
     public var body: some View {
-        Form {
-            Section {
-                TextField("Nome da Cliente", text: $viewModel.clientName)
-                TextField("Telemóvel da Cliente", text: $viewModel.clientPhoneNumber)
-                    .keyboardType(.numberPad)
-            } header: {
-                Text("Detalhes da Cliente")
+        if viewModel.servicesTypes.isEmpty {
+            EmptyView(imageName: "text.page.slash") {
+                Text("Não há nenhum serviço introduzido! \nAdicione um serviço para começar a introduzir as marcações")
             }
-            Section {
-                DatePicker("Data da marcação", selection: $viewModel.appointmentDate, displayedComponents: [.date, .hourAndMinute])
-                
-                TextField("Preço", text: $viewModel.price)
-                    .keyboardType(.numbersAndPunctuation)
-                
-                Picker("Tipo de Marcação", selection: $viewModel.type) {
-                    ForEach(viewModel.servicesTypes, id: \.self) { type in
-                        Text(type)
+        } else {
+            Form {
+                Section {
+                    TextField("Nome da Cliente", text: $viewModel.clientName)
+                    TextField("Telemóvel da Cliente", text: $viewModel.clientPhoneNumber)
+                        .keyboardType(.numberPad)
+                } header: {
+                    Text("Detalhes da Cliente")
+                }
+                Section {
+                    DatePicker("Data da marcação", selection: $viewModel.appointmentDate, displayedComponents: [.date, .hourAndMinute])
+                    
+                    TextField("Preço", text: $viewModel.price)
+                        .keyboardType(.numbersAndPunctuation)
+                    
+                    Picker("Tipo de Marcação", selection: $viewModel.type) {
+                        ForEach(viewModel.servicesTypes, id: \.self) { type in
+                            Text(type)
+                        }
                     }
+                    .pickerStyle(.menu)
+                    Toggle(isOn: $viewModel.inResidence) {
+                        Text("Ir ao domicílio?")
+                    }
+                    .toggleStyle(.checkmark)
+                } header: {
+                    Text("Detalhes da Marcação")
                 }
-                .pickerStyle(.menu)
-                Toggle(isOn: $viewModel.inResidence) {
-                    Text("Ir ao domicílio?")
-                }
-                .toggleStyle(.checkmark)
-            } header: {
-                Text("Detalhes da Marcação")
+                Button(action: {
+                    viewModel.saveAppointment()
+                }, label: {
+                    Text("Guardar")
+                        .foregroundColor(.blue)
+                })
+                .frame(width: 400, height: 30, alignment: .center)
             }
-            Button(action: {
-                viewModel.saveAppointment()
-            }, label: {
-                Text("Guardar")
-                    .foregroundColor(.blue)
-            })
-            .frame(width: 400, height: 30, alignment: .center)
-        }
-        .onAppear {
-            viewModel.fetchData()
+            .onAppear {
+                viewModel.fetchData()
+            }
         }
     }
 }
 
-struct NewAppointmentView_Previews: PreviewProvider {
-    static var previews: some View {
-        NewAppointmentView(
-            viewModel: NewAppointmentViewModel(
-                appointment: Appointment.example,
-                appointmentsPersistenceService: AppointmentPersistenceService(),
-                servicesPersistenceService: AppointmentServicePersistenceService()
-            )
+#Preview {
+    NewAppointmentView(
+        viewModel: NewAppointmentViewModel(
+            appointment: Appointment.example,
+            appointmentsPersistenceService: AppointmentPersistenceService(),
+            servicesPersistenceService: AppointmentServicePersistenceService()
         )
-    }
+    )
 }
