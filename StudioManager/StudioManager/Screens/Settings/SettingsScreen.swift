3897d6f68bcbdb307ef779743c9ed98754283fc1
Dmytro Yaremyshyn <dmytroy@dxspark.com>
2025-05-23 12:42:41 +0100

    Settings screen refactored

--- StudioManager/StudioManager/Screens/Settings/SettingsScreen.swift
+++ StudioManager/StudioManager/Screens/Settings/SettingsScreen.swift
@@ -11,21 +11,48 @@ struct SettingsScreen: View {
     @StateObject var viewModel: SettingsViewModel
 
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-                setting(title: tr.appearanceTitle, image: .dark) {
-                    viewModel.onAppearanceTapped()
-                }
-                
-                setting(title: tr.currencyTitle, image: .dollarCircle) {
-                    viewModel.onCurrencyTapped()
-                }
+        Form {
+            Section {
+                appearance()
+                currency()
             }
+            
+            notifications()
         }
         .navigationTitle(tr.settingsTitle)
     }
+}
+
+private extension SettingsScreen {
+    @ViewBuilder func appearance() -> some View {
+        NavigationLink(destination: Text(tr.appearanceTitle)) {
+            setting(title: tr.appearanceTitle, image: .dark, value: "System") {
+                viewModel.onAppearanceTapped()
+            }
+        }
+    }
     
-    @ViewBuilder private func setting(title: String, image: ImageResource, tap: @escaping () -> Void) -> some View {
+    @ViewBuilder func currency() -> some View {
+        NavigationLink(destination: Text(tr.appearanceTitle)) {
+            setting(title: tr.currencyTitle, image: .dollarCircle, value: "EUR") {
+                viewModel.onCurrencyTapped()
+            }
+        }
+    }
+    
+    @ViewBuilder func notifications() -> some View {
+        Section {
+            NavigationLink(destination: Text("Notifications")) {
+                Text("Notifications")
+            }
+        } footer: {
+            Text("Customize if you wanna be remembered to add new appointments to the app.")
+                .font(.footnote)
+                .foregroundStyle(.gray)
+        }
+    }
+    
+    @ViewBuilder func setting(title: String, image: ImageResource, value: String = "", tap: @escaping () -> Void) -> some View {
         HStack {
             Image(image)
                 .resizable()
@@ -36,24 +63,16 @@ struct SettingsScreen: View {
             Text(title)
                 .font(.callout)
             Spacer()
-            Image(.chevronRight)
-                .resizable()
-                .renderingMode(.template)
-                .scaledToFit()
-                .mediumSize()
-                .foregroundStyle(Color.Text.button)
+            Text(value)
+                .font(.callout)
+                .foregroundStyle(.gray)
         }
-        .padding()
-        .background(
-            RoundedRectangle(cornerRadius: StudioTheme.cr10)
-                .fill(Color(.systemBackground))
-                .shadow(color: .black.opacity(0.1), radius: 4, x: 0, y: 2)
-        )
-        .padding(.horizontal)
         .onTapGesture(perform: tap)
     }
 }
 
 #Preview {
-    SettingsScreen(viewModel: SettingsViewModel())
+    NavigationView{
+        SettingsScreen(viewModel: SettingsViewModel())
+    }
 }
